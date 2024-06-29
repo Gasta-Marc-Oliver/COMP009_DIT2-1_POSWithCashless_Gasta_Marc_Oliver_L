91ad2650f58edd13b54ef14ef2d5f82d6912ea43
Gasta Marc Oliver <marcolivergastagonzales@gmail.com>
2024-06-30 04:13:38 +0800

    add new java files for the project

---
+++ Product.java
@@ -0,0 +1,13 @@
+
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Double price;
+    private Integer stock;
+
+}
